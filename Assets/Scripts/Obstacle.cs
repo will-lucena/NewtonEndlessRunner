a527c8518f8415b1b35fa4c3e181b4c78f143075
Will <will.lucen4@gmail.com>
2018-12-14 00:04:38 -0300

    Adicionado efeito de particulas para o arrastar dos objetos

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obstacle : MonoBehaviour {
+public class Obstacle : MonoBehaviour
+{
+    public float duration;
+    private new ParticleSystem particleSystem;
 
-	public void pushTo(Vector3 position)
+    private void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    public void pushTo(Vector3 position)
 	{
-		transform.position = position;
-	}
+        particleSystem.Play();
+        Vector3 startPos = transform.position;
+        transform.position = Vector3.Lerp(startPos, position, duration);
+        particleSystem.Stop();
+    }
 }
